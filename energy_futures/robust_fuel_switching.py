74b5a36afe4daffb29a4b9139886c0e21bdc77e7
Tom Savage <tom@crvstech.com>
2022-06-01 14:21:34 +0100

    added fuel switching case study

--- energy_futures/robust_fuel_switching.py
+++ energy_futures/robust_fuel_switching.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-# from pyomo.environ import (
-#     log,
-#     TerminationCondition,
-#     ConcreteModel,
-#     Set,
-#     Var,
-#     Reals,
-#     ConstraintList,
-#     Objective,
-#     minimize,
-#     SolverFactory,
-#     value,
-#     maximize,
-# )
+from pyomo.environ import (
+    log,
+    TerminationCondition,
+    ConcreteModel,
+    Set,
+    Var,
+    Reals,
+    ConstraintList,
+    Objective,
+    minimize,
+    SolverFactory,
+    value,
+    maximize,
+)
 import os
 import logging
 
@@ -21,193 +21,307 @@ logging.getLogger("pyomo.core").setLevel(logging.ERROR)
 os.system("clear")
 
 data = pd.read_csv("data/fuel_switching_csv.csv", skiprows=6)[:355]
-boiler_names = [i.replace("\xa0", "") for i in data["Technology"].values[1:]]
 
+boiler_names = [i.replace("\xa0", "") for i in data["Technology"].values[1:]]
 boiler_emissions = [
     float(i.replace(",", ""))
     for i in data["CO2 Emissions from natural gas combustion (kg/yr)"].values[1:]
 ]
+boiler_cost_of_steam = data[
+    "Price of steam produced from Natural gas combustion (£/MWh)"
+].values[1:]
+boiler_energy_required = [
+    float(i.replace(",", ""))
+    for i in data["Thermal Energy required from fuel (MJ/year)"].values[1:]
+]
 boilers = len(boiler_names)
 
 
-# p = {}
-# p["capture_rate"] = {"val": 0.63, "unc": 0.05}
-# p["initial_captured"] = {"val": 10000000, "unc": 1000}
-# p["learning_rate"] = {"val": 0.18, "unc": 0.1}
-# p["initial_capture_cost"] = {"val": 55.4085, "unc": 1}
-# for i in range(plants):
-#     p[plant_names[i] + ": CO2 Emissions"] = {"val": plant_emissions[i], "unc": 500}
+p = {}
+p["Natural Gas LHV (MJ/kg)"] = {"val": 42, "unc": 1}
+p["Hydrogen LHV (MJ/kg)"] = {"val": 120, "unc": 1}
+p["Natural Gas Price (£/tonne)"] = {"val": 292, "unc": 1}
+p["Hydrogen Price (£/tonne)"] = {"val": 1800, "unc": 1}
+p["Natural Gas Boiler Efficiency"] = {"val": 0.9, "unc": 0.05}
+p["Hydrogen Boiler Efficiency"] = {"val": 0.9, "unc": 0.05}
+p["Natural Gas kgCO2e/MWh"] = {"val": 200, "unc": 1}
+p["Hydrogen kgCO2e/MWh"] = {"val": 20, "unc": 1}
+p["A0"] = {"val": 96071.958, "unc": 10}
+p["LR"] = {"val": 0.07, "unc": 0.01}
+p["UC0"] = {"val": 1800, "unc": 10}
 
-# x = {}
-# x["t"] = [-1e20, 1e20]
-# for plant in plant_names:
-#     x[plant + ": Carbon Tax"] = [0, 100]
-# for plant in plant_names:
-#     x[plant + ": Market Share"] = [0, 1]
+for i in range(boilers):
+    p[boiler_names[i] + ": CO2 Emissions from natural gas combustion (kg/yr)"] = {
+        "val": boiler_emissions[i],
+        "unc": 50,
+    }
+for i in range(boilers):
+    p[
+        boiler_names[i]
+        + ": Price of steam produced from Natural gas combustion (£/MWh)"
+    ] = {"val": boiler_cost_of_steam[i], "unc": 50}
+for i in range(boilers):
+    p[boiler_names[i] + ": Thermal Energy required from fuel (MJ/year)"] = {
+        "val": boiler_energy_required[i],
+        "unc": 50,
+    }
 
-# con_list = []
+x = {}
+x["t"] = [-1e20, 1e20]
+for boiler in boiler_names:
+    x[boiler + ": Renewable hydrogen incentive (£/MWh)"] = [0, 50]
+for boiler in boiler_names:
+    x[boiler + ": Market Share"] = [0, 1]
 
-# def parse_vars(x):
-#     carbon_tax = x[1 : plants + 1]
-#     market_share = x[1 + plants :]
-#     t = x[0]
-#     return carbon_tax,market_share,t
-
-# def parse_params(p):
-#     CR = p[0]
-#     UC0 = p[3]
-#     CO2 = p[4:]
-#     A0 = p[1]
-#     LR = -log(1 - p[2]) / log(2)
-#     return CR,UC0,CO2,A0,LR
-
-
-# def make_c(i):
-#     def c(x, p):
-#         CR,UC0,CO2,A0,LR = parse_params(p)
-#         carbon_tax,market_share,t = parse_vars(x)
-#         CO2_captured = [CO2[j] * CR for j in range(plants)]
-#         plant_cost_at_capture_rate = UC0 * CO2[i]
-#         plant_ctax_impact_on_cost = plant_cost_at_capture_rate - (
-#             carbon_tax[i] * CO2_captured[i]
-#         )
-#         return plant_ctax_impact_on_cost
-
-#     return c
-
-# for i in range(plants):
-#     c = make_c(i)
-#     con_list += [c]
+con_list = []
 
 
-# def make_c(i):
-#     def c(x, p):
-#         CR,UC0,CO2,A0,LR = parse_params(p)
-#         carbon_tax,market_share,t = parse_vars(x)
-#         CO2_captured = [CO2[j] * CR for j in range(plants)]
-#         all_captured = sum(CO2_captured)
-#         plant_market_occupancy = CO2_captured[i] / all_captured
-#         plant_improvement = market_share[i] - plant_market_occupancy
-#         return plant_improvement
-
-#     return c
-
-# for i in range(plants):
-#     c = make_c(i)
-#     con_list += [c]
+def parse_vars(x):
+    incentive = x[1 : boilers + 1]
+    market_share = x[1 + boilers :]
+    t = x[0]
+    return incentive, market_share, t
 
 
-# def minimum_improvement(x, p):
-#     CR,UC0,CO2,A0,LR = parse_params(p)
-#     carbon_tax,market_share,t = parse_vars(x)
-#     CO2_captured = [CO2[j] * CR for j in range(plants)]
-#     all_captured = sum(CO2_captured)
-#     CO2_captured_end = [market_share[j] * all_captured for j in range(plants)]
-#     all_CO2_captured = sum(CO2_captured_end)
-#     A = A0 + all_CO2_captured
-#     UC = UC0 * (A / A0) ** (-LR)
-#     return 0.2 - ((UC0 - UC) / UC0)
+def parse_params(p):
+    NG_LHV = p[0]
+    H_LHV = p[1]
+    NG_price = p[2]
+    H_price = p[3]
+    NG_eff = p[4]
+    H_eff = p[5]
+    NG_rho = p[6]
+    H_rho = p[7]
+    A0 = p[8]
+    LR = -log(1 - p[9]) / log(2)
+    UC0 = p[10]
+    ind = 11
+    boiler_CO2 = p[ind : ind + boilers]
+    ind += boilers
+    boiler_steam_price = p[ind : ind + boilers]
+    ind += boilers
+    boiler_energy = p[ind : ind + boilers]
+    return (
+        NG_LHV,
+        H_LHV,
+        NG_price,
+        H_price,
+        NG_eff,
+        H_eff,
+        NG_rho,
+        H_rho,
+        A0,
+        LR,
+        UC0,
+        boiler_CO2,
+        boiler_steam_price,
+        boiler_energy,
+    )
 
 
-# con_list += [minimum_improvement]
+def make_c(i):
+    def c(x, p):
+        (
+            NG_LHV,
+            H_LHV,
+            NG_price,
+            H_price,
+            NG_eff,
+            H_eff,
+            NG_rho,
+            H_rho,
+            A0,
+            LR,
+            UC0,
+            C,
+            D,
+            E,
+        ) = parse_params(p)
+        O, S, t = parse_vars(x)
+        J = E[i] / (3600 * H_eff)
+        H = E[i] / H_LHV
+        I = H / 1000
+        K = I * UC0
+        M = K / J
+        return (M - O[i]) - D[i]
+
+    return c
 
 
-# def con(x, p):
-#     CR,UC0,CO2,A0,LR = parse_params(p)
-#     carbon_tax,market_share,t = parse_vars(x)
-#     CO2_captured = [CO2[j] * CR for j in range(plants)]
-#     all_captured = sum(CO2_captured)
-#     CO2_captured_end = [market_share[j] * all_captured for j in range(plants)]
-#     all_CO2_captured = sum(CO2_captured_end)
-#     A = A0 + all_CO2_captured
-#     UC = UC0 * (A / A0) ** (-LR)
-#     return (UC0 - UC) - t
+for i in range(boilers):
+    c = make_c(i)
+    con_list += [c]
 
 
-# con_list += [con]
+def make_c(i):
+    def c(x, p):
+        (
+            NG_LHV,
+            H_LHV,
+            NG_price,
+            H_price,
+            NG_eff,
+            H_eff,
+            NG_rho,
+            H_rho,
+            A0,
+            LR,
+            UC0,
+            C,
+            D,
+            E,
+        ) = parse_params(p)
+        O, S, t = parse_vars(x)
+        I = [(E[j] / H_LHV) / 1000 for j in range(boilers)]
+        F = I[i] / sum(I)
+        return S[i] - F
+
+    return c
 
 
-# def obj(x):
-#     t = x[0]
-#     return t
+for i in range(boilers):
+    c = make_c(i)
+    con_list += [c]
 
 
-# def var_bounds(m, i):
-#     return (x[i][0], x[i][1])
+def c(x, p):
+    (
+        NG_LHV,
+        H_LHV,
+        NG_price,
+        H_price,
+        NG_eff,
+        H_eff,
+        NG_rho,
+        H_rho,
+        A0,
+        LR,
+        UC0,
+        C,
+        D,
+        E,
+    ) = parse_params(p)
+    O, S, t = parse_vars(x)
+    I = [(E[j] / H_LHV) / 1000 for j in range(boilers)]
+    U = [S[j] * sum(I) for j in range(boilers)]
+    A = A0 + sum(U)
+    UC = UC0 * (A / A0) ** (-LR)
+    return (UC0 - UC) / UC0 - 0.04
 
 
-# def uncertain_bounds(m, i):
-#     return (p[i]["val"] - p[i]["unc"], p[i]["val"] + p[i]["unc"])
-
-# def build_lower_problem(con):
-#     m = ConcreteModel()
-#     m.p = Set(initialize=p.keys())
-#     m.p_v = Var(m.p, domain=Reals, bounds=uncertain_bounds)
-#     param_vars = [m.p_v[str(i)] for i in p.keys()]
-#     m.obj = Objective(expr=con(x_opt, param_vars), sense=maximize)
-#     return m
+con_list += [c]
 
 
-# epsilon = 1e-6
-# solver = "ipopt"
-# m_upper = ConcreteModel()
-# m_upper.x = Set(initialize=x.keys())
-# m_upper.x_v = Var(m_upper.x, domain=Reals, bounds=var_bounds)
-# p_nominal = [p[key]["val"] for key in p.keys()]
-# x_vars = [m_upper.x_v[i] for i in x.keys()]
-# m_upper.cons = ConstraintList()
-# for con in con_list:
-#     m_upper.cons.add(expr=con(x_vars, p_nominal) <= 0)
-# m_upper.obj = Objective(expr=obj(x_vars), sense=minimize)
-# res = SolverFactory(solver).solve(m_upper)
-# term_con = res.solver.termination_condition
-# if term_con is TerminationCondition.infeasible:
-#     print('Problem is robustly infeasible...')
-#     feasible = False
-#     res = {}
-#     res['robust_solution'] = None
-#     res['nominal_solution'] = None
+def c(x, p):
+    (
+        NG_LHV,
+        H_LHV,
+        NG_price,
+        H_price,
+        NG_eff,
+        H_eff,
+        NG_rho,
+        H_rho,
+        A0,
+        LR,
+        UC0,
+        C,
+        D,
+        E,
+    ) = parse_params(p)
+    O, S, t = parse_vars(x)
+    I = [(E[j] / H_LHV) / 1000 for j in range(boilers)]
+    U = [S[j] * sum(I) for j in range(boilers)]
+    A = A0 + sum(U)
+    UC = UC0 * (A / A0) ** (-LR)
+    return (UC0 - UC) - t
 
-# else:
-#     x_opt = value(m_upper.x_v[:])
-#     x_opt_nominal = x_opt
-#     while True:
-#         robust = True
-#         for con in con_list:
-#             m_lower = build_lower_problem(con)
-#             try:
-#                 SolverFactory(solver).solve(m_lower)
-#                 p_opt = value(m_lower.p_v[:])
-#                 print('Constraint violation: ',value(m_lower.obj),end='\r')
-#                 for k in range(len(p_opt)):
-#                     if p_opt[k] is None:
-#                         p_opt[k] = p_nominal[k]
-#                 if value(m_lower.obj) > epsilon:
-#                     robust = False
-#                     m_upper.cons.add(expr=con(x_vars, p_opt) <= 0)
-#             except ValueError:
-#                 continue
-#         if robust is True:
-#             res = {}
-#             robust_solution = {}
-#             nominal_solution = {}
-#             for v in range(plants):
-#                 robust_solution[list(x.keys())[v]] = x_opt[v]
-#                 nominal_solution[list(x.keys())[v]] = x_opt_nominal[v]
-#             res['robust_solution'] = robust_solution
-#             res['nominal_solution'] = nominal_solution
-#             break
-#         res = SolverFactory(solver).solve(m_upper)
-#         term_con = res.solver.termination_condition
-#         if term_con is TerminationCondition.infeasible:
-#             print('Problem is robustly infeasible...')
-#             res = {}
-#             nominal_solution = {}
-#             for v in range(plants):
-#                 nominal_solution[list(x.keys())[v]] = x_opt_nominal[v]
-#             res['robust_solution'] = None
-#             res['nominal_solution'] = x_opt_nominal
-#             break
-#         x_opt = value(m_upper.x_v[:])
 
-# print(res)
+con_list += [c]
+
+
+def obj(x):
+    t = x[0]
+    return t
+
+
+def var_bounds(m, i):
+    return (x[i][0], x[i][1])
+
+
+def uncertain_bounds(m, i):
+    return (p[i]["val"] - p[i]["unc"], p[i]["val"] + p[i]["unc"])
+
+
+def build_lower_problem(con):
+    m = ConcreteModel()
+    m.p = Set(initialize=p.keys())
+    m.p_v = Var(m.p, domain=Reals, bounds=uncertain_bounds)
+    param_vars = [m.p_v[str(i)] for i in p.keys()]
+    m.obj = Objective(expr=con(x_opt, param_vars), sense=maximize)
+    return m
+
+
+epsilon = 1e-6
+solver = "ipopt"
+m_upper = ConcreteModel()
+m_upper.x = Set(initialize=x.keys())
+m_upper.x_v = Var(m_upper.x, domain=Reals, bounds=var_bounds)
+p_nominal = [p[key]["val"] for key in p.keys()]
+x_vars = [m_upper.x_v[i] for i in x.keys()]
+m_upper.cons = ConstraintList()
+for con in con_list:
+    m_upper.cons.add(expr=con(x_vars, p_nominal) <= 0)
+m_upper.obj = Objective(expr=obj(x_vars), sense=minimize)
+res = SolverFactory(solver).solve(m_upper)
+term_con = res.solver.termination_condition
+if term_con is TerminationCondition.infeasible:
+    print("Problem is robustly infeasible...")
+    feasible = False
+    res = {}
+    res["robust_solution"] = None
+    res["nominal_solution"] = None
+
+else:
+    x_opt = value(m_upper.x_v[:])
+    x_opt_nominal = x_opt
+    while True:
+        robust = True
+        for con in con_list:
+            m_lower = build_lower_problem(con)
+            try:
+                SolverFactory(solver).solve(m_lower)
+                p_opt = value(m_lower.p_v[:])
+                print("Constraint violation: ", value(m_lower.obj), end="\r")
+                for k in range(len(p_opt)):
+                    if p_opt[k] is None:
+                        p_opt[k] = p_nominal[k]
+                if value(m_lower.obj) > epsilon:
+                    robust = False
+                    m_upper.cons.add(expr=con(x_vars, p_opt) <= 0)
+            except ValueError:
+                continue
+        if robust is True:
+            res = {}
+            robust_solution = {}
+            nominal_solution = {}
+            for v in range(len(x)):
+                robust_solution[list(x.keys())[v]] = x_opt[v]
+                nominal_solution[list(x.keys())[v]] = x_opt_nominal[v]
+            res["robust_solution"] = robust_solution
+            res["nominal_solution"] = nominal_solution
+            break
+        res = SolverFactory(solver).solve(m_upper)
+        term_con = res.solver.termination_condition
+        if term_con is TerminationCondition.infeasible:
+            print("Problem is robustly infeasible...")
+            res = {}
+            nominal_solution = {}
+            for v in range(len(x)):
+                nominal_solution[list(x.keys())[v]] = x_opt_nominal[v]
+            res["robust_solution"] = None
+            res["nominal_solution"] = x_opt_nominal
+            break
+        x_opt = value(m_upper.x_v[:])
+
+print(res)
